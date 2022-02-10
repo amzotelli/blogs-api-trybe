2c60ed6400697b6ddbaeb1ccc91d825ff5d2780f
Amanda Zotelli <amzotelli@gmail.com>
2022-02-10 16:47:54 -0300

    Adiciona association no BlogPosts Model

--- models/BlogPosts.js
+++ models/BlogPosts.js
@@ -1,9 +1,17 @@
 module.exports = (sequelize, DataTypes) => {
-  sequelize.define('Blog Posts', {
+  const Posts = sequelize.define('Blog Posts', {
     id: DataTypes.INTEGER,
     title: DataTypes.STRING,
     content: DataTypes.STRING,
     published: DataTypes.DATE,
     updated: DataTypes.DATE,
   });
+
+  Posts.associate = (models) => {
+    Posts.belongsTo(models.Users, { foreignKey: 'userId' });
+    Posts.belongsToMany(models.Categories, {
+      through: 'Blog Posts Categories',
+      foreignKey: 'postId',
+    });
+  };
 };
