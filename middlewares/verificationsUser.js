7b58f568669b10add4dbe83bab7817a6a702ec31
Amanda Zotelli <amzotelli@gmail.com>
2022-02-10 12:34:28 -0300

    inicia as funçoes de verificacao para cadastrar usuario

---
+++ middlewares/verificationsUser.js
@@ -0,0 +1,22 @@
+const verifyName = (req, res, next) => {
+  const { displayName } = req.body;
+  if (displayName.length < 8) {
+    return res.status(400).json(
+      { message: '\'displayName\' length must be at least 8 characters long' },
+    );
+  }
+  next();
+};
+
+const verifyEmail = (req, res, next) => {
+  const { email } = req.body;
+  if (!email.includes('@') || !email.includes('.') || email.length < 8) { 
+    return res.status(400).json({ message: '\'email\' must be a valid email' });
+  }
+  next();
+};
+
+module.exports = {
+  verifyName,
+  verifyEmail,
+};
