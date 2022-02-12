5e6698d0cbb0cfd324b5f9e351683b651fff73dc
Amanda Zotelli <amzotelli@gmail.com>
2022-02-12 10:10:15 -0300

    Adiciona o create no usersController

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -5,6 +5,13 @@ const getAll = async (_req, res) => {
   res.status(200).json(users);
 };
 
+const create = async (req, res) => {
+  const { displayName, email, password, image } = req.body;
+  const user = await User.create(displayName, email, password, image);
+  res.status(201).json(user);
+};
+  
 module.exports = {
   getAll,
+  create,
 };
