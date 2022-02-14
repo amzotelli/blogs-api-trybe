dd48d755588e0247c78ea462f4e7fa5a6707faff
Amanda Zotelli <amzotelli@gmail.com>
2022-02-14 16:11:35 -0300

    escreve funçao para verificar a senha

--- middlewares/verificationsUser.js
+++ middlewares/verificationsUser.js
@@ -19,11 +19,21 @@ function verifyEmail(req, res, next) {
       res.status(400).json({ message: '\'email\' must be a valid email' });
       break;
     default:
-      return next();
+      next();
   }
 }
+
+const verifyPassword = (req, res, next) => {
+  const { password } = req.body;
+  if (!password) res.status(400).json({ message: '\'password\' is required' });
+  if (password.length !== 6) {
+    return res.status(400).json({ message: '\'password\' length must be 6 characters long' });
+  }
+  next();
+};
 
 module.exports = {
   verifyName,
   verifyEmail,
+  verifyPassword,
 };
