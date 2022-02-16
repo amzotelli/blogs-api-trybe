c412c0e885da76535a132c94c3a0c0a611ef4868
Amanda Zotelli <amzotelli@gmail.com>
2022-02-16 10:35:45 -0300

    fazendo verificacoes posts

---
+++ controllers/postsController.js
@@ -0,0 +1,22 @@
+const Post = require('../services/postsService');
+
+const getAll = async (req, res) => {
+  try {
+    const posts = await Post.getAll();
+    return res.status(200).json(posts);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+const create = async (req, res) => {
+  const userId = req.user.id;
+  const post = await Post.create(req.body);
+  if (post.status) return res.status(409).json({ message: post.message });
+  return res.status(201).json({ id: post.id, userId, title: post.title, content: post.content });
+};
+
+module.exports = {
+  getAll,
+  create,
+};
---
+++ middlewares/verificationsPost.js
@@ -0,0 +1,9 @@
+const verifyTitle = async (req, res, next) => {
+  const { title } = req.body;
+  if (!title) return res.status(400).json({ message: '"title" is required' });
+  next();
+};
+
+module.exports = {
+  verifyTitle,
+};
